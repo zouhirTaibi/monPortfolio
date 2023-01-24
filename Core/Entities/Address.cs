456712c65faf927c6be8accec1b1ee29e94a85b3
admin <admin@DESKTOP-LVKPJP3>
2023-01-24 11:21:23 -0800

    Add project files.

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+  public class Address:EntityBase
+    {
+        public string Street { get; set; }
+        public int MyProperty { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Core/Entities/PortfolioItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+public  class PortfolioItem:EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
